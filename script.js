b0ba74783a6fb6c1011196eba3e6d0030e0361a6
UdrescuDenis12 <udrescudenis@yahoo.ro>
2026-04-23 18:45:37 +0300

    Am adaugat o poza

--- script.js
+++ script.js
@@ -46,6 +46,12 @@ const masini = {
     imagine: "poza8.jpg",
     tuning: ["Jante R17", "Ochelar far de ceata lipsa", "Semnalizare dinamica", "Pompa servo urla", "Parbriz spart"]
  },
+
+  "WBAZZZ99ZTS123456": {
+    model: "Bogdan Boganescu",
+    imagine: "poza9.jpg",
+    tuning: ["Dragut", "Simpatic", "De treaba", "Amuzant", "Frumos"]
+  },  
 };
 
 function verificaVIN() {
